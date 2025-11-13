06952b84a4a1549d71ff2ab0717a740337782a16
omokhuduraymond <omokhuduraymond@gmail.com>
2025-11-13 21:37:06 +0200

    Priority

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,29 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower() 
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"{task} is a {priority} task and requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+        else:
+            print("Invalid input for time-bound field.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"{task} is a {priority} task and requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+        else:
+            print("Invalid input for time-bound field.")
+    case "low":
+        if time_bound == "yes":
+            print(f"{task} is a {priority} task and requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+        else:
+            print("Invalid input for time-bound field.")
+    case _:  
+        print("Priority not registered.")
+
