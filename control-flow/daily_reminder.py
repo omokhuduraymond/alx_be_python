a0f999923e3769b2bfba251b533dc7f5a8ad1da0
omokhuduraymond <omokhuduraymond@gmail.com>
2025-11-13 21:42:44 +0200

    Prioritize

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,23 +5,23 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"{task} is a {priority} task and requires immediate attention today!")
+            print(f"Reminder: {task} is a {priority} task and requires immediate attention today!")
         elif time_bound == "no":
-            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+            print(f"Note: {task} is a {priority} task, consider completing it when you have free time.")
         else:
             print("Invalid input for time-bound field.")
     case "medium":
         if time_bound == "yes":
-            print(f"{task} is a {priority} task and requires immediate attention today!")
+            print(f"Reminder: {task} is a {priority} task and requires immediate attention today!")
         elif time_bound == "no":
-            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+            print(f"Note: {task} is a {priority} task, consider completing it when you have free time.")
         else:
             print("Invalid input for time-bound field.")
     case "low":
         if time_bound == "yes":
-            print(f"{task} is a {priority} task and requires immediate attention today!")
+            print(f"Reminder: {task} is a {priority} task and requires immediate attention today!")
         elif time_bound == "no":
-            print(f"{task} is a {priority} task, consider completing it when you have free time.")
+            print(f"Note: {task} is a {priority} task, consider completing it when you have free time.")
         else:
             print("Invalid input for time-bound field.")
     case _:  
