4eabe4411d2bb72300595a554f0c7eb43f276839
omokhuduraymond <omokhuduraymond@gmail.com>
2025-11-23 09:14:04 +0200

    Change

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,30 +1,50 @@
 def display_menu():
     print("Shopping List Manager")
-    print ("1. Add Item")
+    print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
 
 def main():
     shopping_list = []
+
     while True:
         display_menu()
-        choice = input("Enter Your choice: ")
-        if choice == '1':
+        
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+            continue
+
+        if choice == 1:
             item = input("Add item to your shopping list: ")
             shopping_list.append(item)
-        elif choice == '2':
+            print(f"'{item}' added!\n")
+
+        elif choice == 2:
             item = input("Enter item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
+                print(f"'{item}' removed!\n")
             else:
-                print("This item cannot be found in your list")
-        elif choice == '3':
-            print("Current List", shopping_list)
-        elif choice == '4':
+                print("This item cannot be found in your list\n")
+
+        elif choice == 3:
+            if not shopping_list:
+                print("Your shopping list is empty.\n")
+            else:
+                print("Current List:")
+                for i, item in enumerate(shopping_list, 1):
+                    print(f"{i}. {item}")
+                print()
+
+        elif choice == 4:
             print("Goodbye!")
             break
+
         else:
-            print("Invalid choice.Please try again")
+            print("Invalid choice. Please try again.\n")
+
 if __name__ == "__main__":
     main()
