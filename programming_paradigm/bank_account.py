1e298f9efdce802ddb5f065e8422a9752ae9a06e
omokhuduraymond <omokhuduraymond@gmail.com>
2025-11-30 15:18:11 +0200

    uodate

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,5 @@ class BankAccount:
         return True
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
-        return self.account_balance
+     print(f"Current Balance: ${self.account_balance:.2f}")
+     self.account_balance
