c0b8ac3111deeff9d03f61e1991d50c8c77441dd
omokhuduraymond <omokhuduraymond@gmail.com>
2025-11-30 15:50:44 +0200

    updat git commit -m updat

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,37 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -5), -10)
+        self.assertEqual(self.calc.add(0, 100), 100)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+        self.assertEqual(self.calc.subtract(10, 20), -10)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-3, 3), -9)
+        self.assertEqual(self.calc.multiply(-4, -2), 8)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+
+    def test_division_by_zero(self):
+        self.assertIsNone(self.calc.divide(10, 0))
+        self.assertIsNone(self.calc.divide(-5, 0))
+
+if __name__ == "__main__":
+    unittest.main()
